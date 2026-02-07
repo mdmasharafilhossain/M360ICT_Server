39b04d0cbf8b205a94436d758eb910b34cee71d6
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-07 12:01:36 +0600

    functionality updated

--- src/database/migrations/hr_users.ts
+++
@@ -1,9 +0,0 @@
-export async function up(knex:any) {
-  return knex.schema.createTable("hr_users", (t:any) => {
-    t.increments("id").primary();
-    t.string("email").unique().notNullable();
-    t.string("password_hash").notNullable();
-    t.string("name").notNullable();
-    t.timestamps(true, true);
-  });
-}
