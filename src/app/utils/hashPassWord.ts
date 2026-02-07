4a81168541f89a167664d0df6b5195203295f036
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-07 21:16:36 +0600

    hashPassword functionality Added

---
+++ src/app/utils/hashPassWord.ts
@@ -0,0 +1,6 @@
+import bcrypt from "bcryptjs";
+
+export const hash = (password: string) => bcrypt.hash(password, process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10  );
+
+export const compare = (password: string, hashPass: string) =>
+  bcrypt.compare(password, hashPass);
