8f573c0a93f3b2b3fedaa09dabffdceeb1c473ca
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-07 21:13:12 +0600

    jwt verify file ts added

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,10 +1,4 @@
-import jwt from "jsonwebtoken";
-
-export interface JwtPayload {
-  id: number;
-  email: string;
-}
-
+import jwt, { JwtPayload } from "jsonwebtoken";
 export function signJwt(payload: JwtPayload) {
   return jwt.sign(payload, process.env.JWT_SECRET!, {
     expiresIn: process.env.JWT_EXPIRES_IN || "1d"
